64ff9017f22189a7e39dca49c6ba3a3e2852ab22
mrios <riosmatias.zarate@gmail.com>
2021-01-08 01:16:30 -0300

    v3: fix video position on mobile, add QR image

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,6 +1,6 @@
 export const Video = (): JSX.Element => {
   return (
-    <div className="video-container h-screen">
+    <div className="video-container">
       <video controls autoPlay>
         <source src="assets/taniatrigo-reel.mp4" type="video/mp4" />
         Your browser does not support the video tag
